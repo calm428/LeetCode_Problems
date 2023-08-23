d195771993a9fcc136eee3e89549f5990d5f0470
calm428 <ollie21428@gmail.com>
2023-08-23 21:46:02 +0500

    Create 21. Remove-Duplicates-from-Sorted-Array.cpp

---
+++ 21. Remove-Duplicates-from-Sorted-Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums){  
+    //using one variable that points to the initial unique element
+    int index=0;
+    for(int i=1;i<nums.size();i++){
+        if(nums[i]!=nums[i-1]){
+            nums[index+1]=nums[i];
+            index++;
+        }
+    }
+    return index+1;
+    }
+};
